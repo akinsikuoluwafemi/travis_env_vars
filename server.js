ed1397cb0e76a6fa4a14fa489453c48c0d35bccb
Femi Akinsiku <akinsiku13@gmail.com>
2025-05-08 22:29:28 +0100

    chore: add steps to add env variable

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// to save env variables, you dont need an env file, just do this in your terminal:
+// export MY_NAME=Femi
+
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const myName = process.env.MY_NAME;
+const myName = process.env.MY_NAME;  // this will be undefined if you don't set it in your terminal: use it like this
+
+// set it in dockerfile:
+// # Set the environment variable NODE_ENV to production
+// ARG MY_NAME
+// ENV MY_NAME=$MY_NAME
 
 async function main() {
   while (true) {
